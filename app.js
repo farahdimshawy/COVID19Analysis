f64f4b7c458e31a4afa9e2f2a7e9cbd25e45eeed
Farah <148490880+farahdimshawy@users.noreply.github.com>
2025-05-02 18:48:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const url = "https://public.tableau.com/views/COVID_17461991248990/COVID-19GLOBA
 const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
-    height: 60000,
-    width: 60000,
+    height: 15000,
+    width: 15000,
     onFirstInteraction: function() {
         workbook = viz.getWorkbook();
         activeSheet = workbook.getActiveSheet();
